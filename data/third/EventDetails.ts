b761e04d061a13a13bf47522c40052dbb2cf0249
junny97 <pcohad12@gmail.com>
2024-10-30 23:34:31 +0900

    design: 배너 레이아웃 수정

--- data/third/EventDetails.ts
+++ data/third/EventDetails.ts
@@ -1,5 +1,7 @@
 export const eventDetails = {
   name_ko: '테오의 컨퍼런스 3기',
   name_en: 'TeoConf',
-  dateAndLocation: '24.11.23 토요일\n24.11.24 일요일\n팀 스파르타',
+  date_saturday: '24.11.23 토요일',
+  date_sunday: '24.11.24 일요일',
+  location: '아이콘역삼빌딩',
 }
