03d78382049a6c6dbe952d14ec0f3a29ed6b07f2
조혜린 <hyer1205@gmail.com>
2025-11-01 16:36:02 +0900

    feat: 모아보기 섹션에 실제 data 추가
    - next.config.js -> 콘텐츠 썸네일 적용하기 위한 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,34 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'velog.velcdn.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img1.daumcdn.net',
+        port: '',
+        pathname: '/thumb/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blog.kakaocdn.net',
+        port: '',
+        pathname: '/dna/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
