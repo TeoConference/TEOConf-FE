0fa58d8d46750c8f262025df73e4810543ee012c
조혜린 <hyer1205@gmail.com>
2025-11-11 12:03:31 +0900

    feat: 연사자 이미지 수정 및 session list 모바일 버전 UI 1차 완료

--- components/2025-commingsoon/sections/Sessions.tsx
+++ components/2025-commingsoon/sections/Sessions.tsx
@@ -9,10 +9,10 @@ const Sessions = () => {
   return (
     <section
       id="sessions"
-      className="relative w-full overflow-hidden py-[6.25rem] px-10 tablet:py-24 tablet:px-8 desktop:px-12 bg-purple-800"
+      className="relative w-full overflow-hidden py-[6.25rem] tablet:py-24 bg-purple-800"
     >
-      {/* 제목 */}
-      <div className="flex flex-col gap-4 p-2 tablet:p-0 mb-10 tablet:mb-12 ">
+      {/* Title */}
+      <div className="flex flex-col gap-4 px-10 tablet:px-8 desktop:px-12 mb-10 tablet:mb-12 ">
         <div className="w-full flex-center flex-col">
           <h2 className="w-full text-white text-[2rem] tablet:text-[3.5rem] desktop:text-[3rem] font-bold text-center max-w-4xl ">
             테오콘{' '}
@@ -20,9 +20,9 @@ const Sessions = () => {
           </h2>
         </div>
       </div>
-      {/* 내용 영역 */}
-      <div className="mx-auto max-w-md">
-        <div className="relative bg-purple-600/50 rounded-full flex h-[2.875rem]">
+      {/* Tab Switch */}
+      <div className="mx-auto px-10 tablet:px-8 desktop:px-12 flex justify-center">
+        <div className="relative bg-purple-600/50 rounded-full flex h-[2.875rem] w-[17.375rem]">
           {/* 슬라이딩 흰색 배경 */}
           <div
             className="absolute top-0 left-0 w-1/2 h-full bg-white rounded-full transition-transform duration-300 ease-out"
@@ -51,6 +51,36 @@ const Sessions = () => {
             12월 7일
           </button>
         </div>
+      </div>
+      {/* Sessions */}
+      <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-6 px-4 tablet:px-8 desktop:px-12 mt-6">
+        {sessions[activeTab].speakers.map((speaker, index) => (
+          <article
+            key={index}
+            className={clsx(
+              'w-full max-w-[20.625rem] mx-auto p-6 rounded-xl flex flex-col gap-4',
+              activeTab === 0 ? 'bg-purple-50' : 'bg-purple-100'
+            )}
+          >
+            <p className="text-sm font-bold text-[#FF383C]">
+              # {speaker.track}
+            </p>
+            <h3 className="text-xl font-bold text-purple-600 break-keep">
+              {speaker.title}
+            </h3>
+            <div className="flex gap-12">
+              <div className="flex-1 text-purple-600">
+                <p className="text-base font-bold">{speaker.name}</p>
+                <span className="text-sm">{speaker.role}</span>
+              </div>
+              <Image
+                className="w-12 h-12 rounded-full object-cover bg-white"
+                src={speaker.image}
+                alt={speaker.name}
+              />
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   )
