baa9b75b676e26be290dbe9e62ef2f9222caa7fb
조혜린 <hyer1205@gmail.com>
2025-11-11 23:16:58 +0900

    fix: 신청폼 링크와 신청하기 버튼 연결

--- components/2025-commingsoon/sections/Hero.tsx
+++ components/2025-commingsoon/sections/Hero.tsx
@@ -152,11 +152,12 @@ const Hero = () => {
               {/* 참가 신청 버튼 */}
               <div className="w-full px-12 tablet:px-16 flex-center">
                 <button
-                  // disabled
-                  className="w-[476px] desktop:w-full text-[2rem] desktop:text-[1.25rem] bg-[#969497] text-white px-6 py-2 tablet:px-8 tablet:py-5.5 rounded-2xl text-sm tablet:text-base font-bold transition-colors duration-200 h-[86px] desktop:h-[64px] disabled:cursor-not-allowed"
-                  // className="w-[476px] desktop:w-full text-[2rem] desktop:text-[1.25rem] bg-purple-500 hover:bg-[#9333EA] text-white px-6 py-2 tablet:px-8 tablet:py-2.5 rounded-2xl tablet:text-base font-bold transition-colors duration-200 h-[69px]"
+                  onClick={() => {
+                    window.open('https://forms.gle/q2ThbtPYesLS49PB9', '_blank')
+                  }}
+                  className="w-[476px] desktop:w-full text-[2rem] desktop:text-[1.25rem] bg-purple-500 hover:bg-[#9333EA] text-white px-6 py-2 tablet:px-8 tablet:py-2.5 rounded-2xl tablet:text-base font-bold transition-colors duration-200 h-[69px]"
                 >
-                  신청이 곧 시작됩니다.
+                  참가 신청
                 </button>
               </div>
             </div>
@@ -166,11 +167,12 @@ const Hero = () => {
         {/* 모바일용 참가 신청 버튼 */}
         <div className="w-full max-w-[330px] mx-auto tablet:hidden">
           <button
-            // className="w-full text-[1.5rem] bg-purple-500 hover:bg-purple-600 text-white px-6 py-2 rounded-2xl font-bold transition-colors duration-200 h-[69px]"
-            // disabled
-            className="w-full text-[1.5rem] bg-[#969497] text-white px-6 py-2 rounded-2xl font-bold transition-colors duration-200 h-[69px] disabled:cursor-not-allowed"
+            onClick={() => {
+              window.open('https://forms.gle/q2ThbtPYesLS49PB9', '_blank')
+            }}
+            className="w-full text-[1.5rem] bg-purple-500 hover:bg-purple-600 text-white px-6 py-2 rounded-2xl font-bold transition-colors duration-200 h-[69px]"
           >
-            신청이 곧 시작됩니다.
+            참가 신청
           </button>
         </div>
       </div>
