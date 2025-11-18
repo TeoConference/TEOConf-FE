42442853d7b35e1e5db601f6d360fc11834312ea
Lucy <hyer1205@gmail.com>
2025-11-18 17:40:58 +0900

    refactor: Sessions 폴더 이동(shared -> sections)

--- pages/2025/index.tsx
+++ pages/2025/index.tsx
@@ -6,7 +6,7 @@ import Overview from '@/components/2025-commingsoon/sections/Overview'
 import Sponsor from '@/components/2025-commingsoon/sections/Sponsor'
 import SNS from '@/components/2025-commingsoon/sections/SNS'
 import Footer from '@/components/2025-commingsoon/sections/Footer'
-import Sessions from '@/components/2025-commingsoon/shared/Sessions'
+import Sessions from '@/components/2025-commingsoon/sections/Sessions'
 import Map from '@/components/2025-commingsoon/sections/Map'
 
 export default function Home() {
