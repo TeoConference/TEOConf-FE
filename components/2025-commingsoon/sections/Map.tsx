24b3b01e639677c1faa34c5c172c7376a7213abe
Lucy <hyer1205@gmail.com>
2025-11-18 16:34:19 +0900

    feat: 커스텀 마커 & 커스텀 오버레이 생성

--- components/2025-commingsoon/sections/Map.tsx
+++ components/2025-commingsoon/sections/Map.tsx
@@ -17,14 +17,44 @@ const Map = () => {
 
       const map = new window.kakao.maps.Map(mapContainer.current!, options)
 
+      // 커스텀 마커 이미지 설정 (원본 54x64의 1.5배)
+      const imageSrc = '/images/2025/map/custom-marker.png'
+      const imageSize = new window.kakao.maps.Size(81, 96)
+      const imageOption = { offset: new window.kakao.maps.Point(40.5, 96) }
+
+      const markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      )
+
       const markerPosition = new window.kakao.maps.LatLng(
         LOCATION.lat,
         LOCATION.lng
       )
+
       const marker = new window.kakao.maps.Marker({
         position: markerPosition,
+        image: markerImage,
       })
       marker.setMap(map)
+
+      // 커스텀 오버레이 설정
+      const content = `
+        <div class="relative bottom-[115px] bg-white rounded-md shadow-lg px-4 py-2 border border-gray-300 text-xl">
+          <a href="https://map.kakao.com/link/map/74968626" target="_blank" rel="noopener noreferrer" class="text-gray-800 no-underline hover:underline font-bold text-sm">
+            엘리스랩 성수
+          </a>
+          <div class="absolute left-1/2 -translate-x-1/2 -bottom-[12px] w-0 h-0 border-l-[11px] border-l-transparent border-r-[11px] border-r-transparent border-t-[12px] border-t-white"></div>
+        </div>
+      `
+
+      new window.kakao.maps.CustomOverlay({
+        map: map,
+        position: markerPosition,
+        content: content,
+        yAnchor: 1,
+      })
     })
 
     return () => setIsKakaoLoaded(false)
