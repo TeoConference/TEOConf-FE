8f644d17104ea738671604104ca13e85421e198f
Lucy <hyer1205@gmail.com>
2025-11-18 16:45:59 +0900

    feat: 커스텀 오버레이에 '길찾기' 추가
    - 장소 ID 매직넘버 처리

--- components/2025-commingsoon/sections/Map.tsx
+++ components/2025-commingsoon/sections/Map.tsx
@@ -41,9 +41,12 @@ const Map = () => {
 
       // 커스텀 오버레이 설정
       const content = `
-        <div class="relative bottom-[115px] bg-white rounded-md shadow-lg px-4 py-2 border border-gray-300 text-xl">
-          <a href="https://map.kakao.com/link/map/74968626" target="_blank" rel="noopener noreferrer" class="text-gray-800 no-underline hover:underline font-bold text-sm">
+        <div class="relative bottom-[115px] bg-white rounded-md shadow-lg px-4 py-2 border border-gray-300 flex flex-col items-center">
+          <a href="https://map.kakao.com/link/map/${LOCATION.placeId}" target="_blank" rel="noopener noreferrer" class="text-gray-800 no-underline hover:underline font-bold text-lg">
             엘리스랩 성수
+          </a>
+          <a href="https://map.kakao.com/link/to/${LOCATION.placeId}" target="_blank" rel="noopener noreferrer" class="w-full mt-2 px-3 py-2 bg-purple-400 text-white text-xs font-semibold rounded hover:bg-purple-500 no-underline text-center">
+            길찾기
           </a>
           <div class="absolute left-1/2 -translate-x-1/2 -bottom-[12px] w-0 h-0 border-l-[11px] border-l-transparent border-r-[11px] border-r-transparent border-t-[12px] border-t-white"></div>
         </div>
