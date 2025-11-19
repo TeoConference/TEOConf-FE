d6046eec7f5f3547f0f6735d79587ac5cda86f80
Lucy <hyer1205@gmail.com>
2025-11-19 14:45:03 +0900

    remove: 불필요한 주석 제거

--- components/2025-commingsoon/sections/Map/useKakaoMap.ts
+++ components/2025-commingsoon/sections/Map/useKakaoMap.ts
@@ -27,25 +27,20 @@ export const useKakaoMap = (
 
       let overlay = createCustomOverlay(markerPosition, LOCATION, map)
 
-      // 화면 resize 시 마커와 오버레이를 다시 생성
       const handleResize = () => {
-        // 기존 마커와 오버레이 제거
         marker.setMap(null)
         overlay.setMap(null)
 
-        // 새로운 마커와 오버레이 생성
         marker = createCustomMarker(markerPosition)
         marker.setMap(map)
 
         overlay = createCustomOverlay(markerPosition, LOCATION, map)
 
-        // 중앙 재설정
         map.setCenter(markerPosition)
       }
 
       window.addEventListener('resize', handleResize)
 
-      // cleanup 함수로 이벤트 리스너 제거
       return () => {
         window.removeEventListener('resize', handleResize)
       }
